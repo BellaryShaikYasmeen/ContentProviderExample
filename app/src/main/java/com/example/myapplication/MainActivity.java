220bc91bf005dd4fd2dc94ef092a86553adf8d64
Yasmeen Shaik <bellaryshaikyasmeen@gmail.com>
2023-07-13 03:56:41 +0530

    Refactored the Code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,16 @@
 package com.example.myapplication;
 
-import android.annotation.TargetApi;
+import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.ContentResolver;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -27,96 +23,45 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv1=(TextView)findViewById(R.id.textView);
-        tv1.setOnClickListener(
-                v ->requestContactPermission()
-        );
+        tv1 = findViewById(R.id.textView);
+        tv1.setOnClickListener(v -> requestContactPermission());
 
     }
+
     private void getContacts() {
         //TODO get contacts code here
         Toast.makeText(this, "Get contacts ....", Toast.LENGTH_LONG).show();
-        ContentResolver cr=getContentResolver();
-        Uri uri= ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        Cursor cursor=cr.query(uri,null,null,null);
-        Log.i("Contacts_provider","Totalno of conatcts"+Integer.toString(cursor.getCount()));
-        if(cursor.getCount()>0)
-        {
-            while (cursor.moveToNext())
-            {
-                String contactName= cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String contactNo= cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                Log.i("Contacts_provider","\n ContactName \n"+contactName+"\n Phone Number\n"+contactNo);
+        ContentResolver cr = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = cr.query(uri, null, null, null);
+        Log.i("Contacts_provider", "Total no of Contacts" + cursor.getCount());
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String contactName = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String contactNo = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                Log.i("Contacts_provider", "\n ContactName \n" + contactName + "\n Phone Number\n" + contactNo);
             }
 
         }
-
 
 
     }
+
     public void requestContactPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        android.Manifest.permission.READ_CONTACTS)) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("Read Contacts permission");
-                    builder.setPositiveButton(android.R.string.ok, null);
-                    builder.setMessage("Please enable access to contacts.");
-                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @TargetApi(Build.VERSION_CODES.M)
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            requestPermissions(
-                                    new String[]
-                                            {android.Manifest.permission.READ_CONTACTS}
-                                    , PERMISSIONS_REQUEST_READ_CONTACTS);
-                        }
-                    });
-                    builder.show();
-                } else {
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{android.Manifest.permission.READ_CONTACTS},
-                            PERMISSIONS_REQUEST_READ_CONTACTS);
-                }
-            } else {
-                getContacts();
-            }
-        } else {
-            getContacts();
-        }
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 0);
+        getContacts();
+
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_READ_CONTACTS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getContacts();
-                } else {
-                    Toast.makeText(this, "You have disabled a contacts permission", Toast.LENGTH_LONG).show();
-                }
-                return;
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getContacts();
+            } else {
+                Toast.makeText(this, "You have disabled a contacts permission", Toast.LENGTH_LONG).show();
             }
         }
     }
 }
-/*
-Uri uri= ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        Cursor cursor=cr.query(uri,null,null,null);
-        Log.i("Contacts_provider","Totalno of conatcts"+Integer.toString(cursor.getCount()));
-        if(cursor.getCount()>0)
-        {
-            while (cursor.moveToNext())
-            {
-                String contactName= cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String contactNo= cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
-              Log.i("Contacts_provider","ConatctName"+contactName+"Phone Number"+contactNo);
-            }
-
-        }
-
-*/
-
